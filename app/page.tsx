f2b3584e5cd38e4d426be93104f0e2fc7908b1c0
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-01-25 12:35:19 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-2 sm:p-20">
-      <Button>Continue</Button>
+      <Link href={"/documents/124"} className={cn(buttonVariants())}>
+        Continue to dashboard
+      </Link>
       <Button variant={"outline"}>Cancel</Button>
     </div>
   );
