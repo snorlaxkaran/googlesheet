32f47ddf15840b96413204f255c36ad0446565be
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-01-25 12:37:57 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-2 sm:p-20">
-      <Link href={"/documents/124"} className={cn(buttonVariants())}>
-        Continue to dashboard
+      <Link href={"/documents/123"} className={cn(buttonVariants())}>
+        Continue to documents.1234
       </Link>
       <Button variant={"outline"}>Cancel</Button>
     </div>
